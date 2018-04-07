fb749682affdffe8b6222f5edf5f5275d8d72860
yyusuke <yyusuke27@gmail.com>
2018-04-07 17:19:01 +0000

    add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "staging"
   protect_from_forgery with: :exception
     def hello
         render html: "hello, world!"

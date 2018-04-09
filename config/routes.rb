be45d68175b86b59c1bbd748f88366c2406da54a
yyusuke <yyusuke27@gmail.com>
2018-04-09 01:26:53 +0000

    change top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
-  get '/signup', to:'users#new'
+  root 'users#new'
   resources :users
 end

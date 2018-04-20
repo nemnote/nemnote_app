88cd1aa32790c25445855afcc9489f9d145bed8c
yyusuke27 <yyusuke27@gmail.com>
2018-04-21 00:58:32 +0900

    add S3

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,13 +2,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fill: [200, 200]
 
-   if Rails.env.production?
+   if Rails.env.production? || Rails.env.staging?
      storage :fog
    else
      storage :file
    end
-
-  storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"

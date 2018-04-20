6c435576a9a7d6be3b00b87d8131dd8f77f92712
yyusuke27 <yyusuke27@gmail.com>
2018-04-20 17:51:25 +0900

    remove application.js require jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,4 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require jquery
-//= require jquery_ujs
 //= require_tree .
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < UsersController
     before_action :logged_in_user, only: [:create, :destroy]
+    before_action :correct_user,   only: :destroy
 
     def new
         @note = Note.new
